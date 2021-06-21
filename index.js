263dc0766db30c8fd5cb86e3a0e884df19f870a3
Viktoria <doduchenko29@gmail.com>
2021-06-21 18:23:34 +0300

    add

---
+++ index.js
@@ -0,0 +1,12 @@
+const pinger = (num, period) => {
+  let i = num;
+  console.log('Ping');
+  const interval = setInterval(() => {
+    if(i-- > 0) {
+      console.log('Ping')
+    } else {
+      clearInterval(interval)
+    }
+  }, period)
+}
+pinger(5, 1000)
